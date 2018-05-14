b72e7c4e95edd7760978e07ab45bd1f8819efc13
Mathieu Gandin <mathieu.gandin@gmail.com>
2018-05-14 14:35:09 +0200

    install Axios and make an http request

--- src/main/reactjs/Hello.tsx
+++ src/main/reactjs/Hello.tsx
@@ -1,8 +1,16 @@
 import * as React from "react";
 
+import axios from 'axios'
+
 export interface HelloProps { compiler: string; framework: string; }
 
 export class Hello extends React.Component<HelloProps, {}> {
+
+    componentDidMount() {
+        axios.get("http://localhost:8081/disquaires")
+            .then(response => (console.log(response)))
+    }
+
     render() {
         return <h1>Hello from {this.props.compiler} and {this.props.framework}!</h1>;
     }
